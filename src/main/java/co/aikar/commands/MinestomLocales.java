d904306c0491fecc3add2836cefb47957d6539be
Archy-X <andrewxiearchway@gmail.com>
2020-12-28 15:09:25 -0700

    Added acf-minecraft message bundle

--- src/main/java/co/aikar/commands/MinestomLocales.java
+++ src/main/java/co/aikar/commands/MinestomLocales.java
@@ -1,9 +1,17 @@
 package co.aikar.commands;
 
 
+import java.util.Locale;
+
 public class MinestomLocales extends Locales {
 
     public MinestomLocales(MinestomCommandManager manager) {
         super(manager);
+    }
+
+    @Override
+    public void loadLanguages() {
+        super.loadLanguages();
+        addMessageBundle("acf-minecraft", Locale.ENGLISH);
     }
 }
